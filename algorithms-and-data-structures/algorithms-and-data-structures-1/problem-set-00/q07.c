c36573781e1cb905d2c4fd55cac363ec8563b940
Felipe <fsa398975@gmail.com>
2026-07-31 14:17:06 -0300

    Add q07.c for calculating power of radius

---
+++ algorithms-and-data-structures/algorithms-and-data-structures-1/problem-set-00/q07.c
@@ -0,0 +1,9 @@
+        #include<stdio.h>
+        #include<math.h>
+        int main(){
+                int r,p,pg;
+                scanf("%d %d",&r,&p);
+                pg=p*pow(r,4);
+                printf("%d",pg);
+
+        }
