81334b447f1da448b21a6947a7451031d5824a42
Felipe <fsa398975@gmail.com>
2026-07-31 14:42:59 -0300

    Delete algorithms-and-data-structures/algorithms-and-data-structures-1/problem-set-07 directory

--- algorithms-and-data-structures/algorithms-and-data-structures-1/problem-set-07/q11.c
+++
@@ -1,9 +0,0 @@
-	#include<stdio.h>
-	int main(){
-		char vet[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		char* p=vet;
-		while(*p!='\0'){
-			printf("%c ",*p);
-			p++;
-		}
-	}
--- algorithms-and-data-structures/algorithms-and-data-structures-1/problem-set-07/representacaoVetor.c
+++
@@ -1,5 +0,0 @@
-vet[3]={1,2,3};
-vet==&vet[0]==&(*(vet+0))==vet+0==endereco da primeira posicao
-vet[0]==*(vet+0)==*vet
-vet[1]==*(vet+1)==1[vet]==*(1+vet)==2
-vet==&vet[0] -> &vet==endereco de todo o vetor
