379803e8252ce48a30cf3da973a1870e0d8f58b2
Felipe <fsa398975@gmail.com>
2026-07-31 14:17:59 -0300

    Add q09.c to calculate decimal from fraction

---
+++ algorithms-and-data-structures/algorithms-and-data-structures-1/problem-set-00/q09.c
@@ -0,0 +1,7 @@
+        #include<stdio.h>
+        int main(){
+                int numerador,denominador;
+                scanf("%d %d",&numerador,&denominador);
+                float decimal=(float)numerador/(float)denominador;
+                printf("%.6f",decimal);
+        }
