56224d0ae3a9eed7aeb0920a25e78a5b820ec52b
Felipe <fsa398975@gmail.com>
2026-07-31 14:11:56 -0300

    Add program to reverse a three-digit number
    Implement a program to reverse a three-digit number.

---
+++ algorithms-and-data-structures/algorithms-and-data-structures-1/problem-set-00/q01.c
@@ -0,0 +1,12 @@
+    #include<stdio.h>
+        int main(){
+                int n,c,d,u,novoN;
+                scanf("%d",&n);
+                if(n>=100 && n<1000){
+                        u=n/100;
+                        d=(n/10)%10;
+                        c=n%10;
+                        novoN=(c*100)+(d*10)+u;
+                        printf("%d",novoN);
+                }
+        }
