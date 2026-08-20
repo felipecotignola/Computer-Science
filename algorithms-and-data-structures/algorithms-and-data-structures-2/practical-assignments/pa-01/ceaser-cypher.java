149661f850edbc480957c1decd946354830002bd
Felipe Cotignola <fsa398975@gmail.com>
2026-08-20 09:36:02 -0300

    atualizacoes

---
+++ algorithms-and-data-structures/algorithms-and-data-structures-2/practical-assignments/pa-01/ceaser-cypher.java
@@ -0,0 +1,14 @@
+	import java.util.Scanner;
+	public String cypher(String string){
+		for(int i=0;i<string.length();i++){
+			char c=(string.charAt(i))+3;
+			string.charAt(i)=c;
+		}
+		return string;	
+	}
+	public class caeser-cypher{
+		public static void main(String[] args){
+			Scanner sc=new Scanner(System.in);
+				
+		}
+	}
