1d2456b06e90b07162655fd87c6f6b3b2040fd01
Felipe Cotignola <fsa398975@gmail.com>
2026-08-21 08:04:24 -0300

    q08

---
+++ algorithms-and-data-structures/algorithms-and-data-structures-2/practical-assignments/pa-01/passwordValidation.java
@@ -0,0 +1,42 @@
+	import java.util.Scanner;
+	public boolean validation(String string){
+		if(string.length()>=8){
+			int upperCase=0,lowerCase=0,number=0,specialChar=0;
+			for(int i=0;i<string.length();i++){
+				if(string.charAt(i)>=65 && string.charAt(i)<=90){
+					upperCase++;
+				}
+				else if(string.charAt(i)>=97 && string.charAt(i)<=122){
+					lowerCase++;
+				}
+				else if(string.charAt(i)>=48 && string.charAt(i)<=57){
+					number++;
+				}
+				else{
+					specialChar++;
+				}
+			}
+			if(upperCase!=0 && lowerCase!=0 && number!=0 && specialChar!=0){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+	}
+	public class passwordValidation{
+		public static void main(String[] args){
+			Scanner sc=new Scanner(System.in);
+			String string=sc.next;
+			if(validation(string)){
+				System.out.printf("SIM\n");
+			}
+			else{
+				System.out.printf("NAO\n");
+			}
+			
+		}
+	}
